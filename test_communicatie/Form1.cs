796d0aa17ac72e838b336476e2a145dcdcc6eb4f
Dani <d.kools1@outlook.com>
2019-05-09 08:53:51 +0200

    Shows the teams

---
+++ test_communicatie/Form1.cs
@@ -0,0 +1,37 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace test_communicatie
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            System.Net.WebClient downloader = new System.Net.WebClient();
+            string teamJson;
+
+            teamJson = downloader.DownloadString("http://localhost/fifa-Php/PHP/jsonecho.php");
+
+            List<Team> teams = JsonConvert.DeserializeObject<List<Team>>(teamJson);
+
+            foreach(Team team in teams)
+            {
+                listBox1.Items.Add(team.teamname);
+            }
+        }
+    }
+}
